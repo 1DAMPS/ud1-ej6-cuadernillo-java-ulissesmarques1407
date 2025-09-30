ff31d1a2d2263476ec602ed091a296e5cd0885c2
ulissesmarques1407 <ulisses.marques.de.al@iespoligonosur.org>
2025-09-30 17:15:53 +0000

    Ejercicio dos

---
+++ EjercicioDos.java
@@ -0,0 +1,6 @@
+public class EjercicioDos {
+    public static void main (String[] args){
+        System.out.println("Me llamo Ulisses");
+        System.out.println("Este es mi segundo programa en Java");
+    }
+}
