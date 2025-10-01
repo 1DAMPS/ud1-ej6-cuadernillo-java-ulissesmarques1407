6a4feb81e064682fbcd7e7406e2688ba4663b170
ulissesmarques1407 <ulisses.marques.de.al@iespoligonosur.org>
2025-10-01 14:54:33 +0000

    Ejercicio tres heccho

---
+++ EjercicioTres.java
@@ -0,0 +1,14 @@
+// importo la librería necesaria para el scanner
+import java.util.Scanner;
+    public class EjercicioTres{
+        public static void main(String [] args){
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Cuál es tu edad?");
+            int edad = sc.nextInt();
+            System.out.println("Tienes " + edad + " años");
+           
+
+
+
+    } 
+    }
--- prueba.java
+++ prueba.java
@@ -1,5 +1,4 @@
-public class prueba {
-    public static void main(String[] args) {
-        System.out.println("Hola mundo");
-    }
-}
+import java.util.Scanner;
+
+public class Prueba
+public static void main String 
