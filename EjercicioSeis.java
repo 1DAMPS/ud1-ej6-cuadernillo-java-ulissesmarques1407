483ae700a11f69a37d28db098067b4e00e2449d7
ulissesmarques1407 <ulisses.marques.de.al@iespoligonosur.org>
2025-10-05 13:19:33 +0000

    Ejercicio seis hecho

---
+++ EjercicioSeis.java
@@ -0,0 +1,15 @@
+
+public class EjercicioSeis{
+    public static void main (String[] args){
+        int añoActual = 2025;
+        int añoNacimiento = 2003;
+        String nombreHermana = "Barbara";
+        int suEdad = añoActual - añoNacimiento;
+        System.out.println("***EJERCICIO SEIS***");
+        System.out.println("Tengo dos hermanas, una se llama " + nombreHermana);
+        System.out.println("ella nació en el año " + añoNacimiento);
+        System.out.println("Ella tiene " + suEdad + " años");
+
+     }
+}
+   
