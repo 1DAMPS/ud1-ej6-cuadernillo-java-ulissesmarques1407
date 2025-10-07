91af0dc6c8c8e3b0a8f6ee7c0843bfb4bc30c04c
ulissesmarques1407 <ulisses.marques.de.al@iespoligonosur.org>
2025-10-07 16:51:13 +0000

    Ejercicio diez hecho

---
+++ EjercicioDiez.java
@@ -0,0 +1,14 @@
+public class EjercicioDiez{ //se crea una clase ejercicio diez 
+    public static void main(String[] args){ /* se declara el método main */
+        
+        byte edadPerro = 8; //*declaro e inicializo las dos variables 
+        short venus = 30000;
+        // siempre saber elegir el tipo de variable según el dato que sen quiera expresar 
+        
+        
+
+        System.out.println("El perro de mi amiga Ángela tiene " + edadPerro + " años"); 
+        System.out.println("La estatua de la Venus de Willendorf tiene aproximadamente " + venus + " años");
+
+    }
+}
