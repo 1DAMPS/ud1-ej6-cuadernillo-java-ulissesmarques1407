2842fe9ec2f1161141b5955cb7d910a9b77ebefb
ulissesmarques1407 <ulisses.marques.de.al@iespoligonosur.org>
2025-10-07 17:15:10 +0000

    Ejercicio DOCE HECHO

---
+++ EjercicioDoce.java
@@ -0,0 +1,14 @@
+public class EjercicioDoce{
+    public static void main (String[] args){
+            float pi = 3.14159f;
+
+            System.out.println("El numero pi es " + pi);
+
+            /* SI NO SE LE PONE LA F:
+             * EjercicioDoce.java:3: error: incompatible types: possible lossy conversion from double to float
+            float pi = 3.14159;
+            */
+    }
+
+
+}
