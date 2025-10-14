361875019e9d118d6c971d041ed089f495289668
ulissesmarques1407 <ulisses.marques.de.al@iespoligonosur.org>
2025-10-14 15:55:39 +0000

    ejercicio cuatorce hecho

---
+++ EjercicioCuatorce.java
@@ -0,0 +1,11 @@
+
+public class EjercicioCuatorce {
+    
+    public static void main (String[] args){
+
+        final int semana = 7; //nuestra constante
+    
+    System.out.println("La semana tiene "+ semana + " días");
+
+    }
+}
