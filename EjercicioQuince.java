7f3ac5546f18ed497de9a2ec9b91ed36021c95cf
ulissesmarques1407 <ulisses.marques.de.al@iespoligonosur.org>
2025-10-14 16:16:09 +0000

    ejercicioquince hecho

---
+++ EjercicioQuince.java
@@ -0,0 +1,7 @@
+public class EjercicioQuince{
+    public static void main(String[] args){
+        int num = 2147483647;
+        num ++;
+        System.out.println("El numero es:" +num ); /*,le da la vuelta, se vuelve negativo!!!! */
+    }
+}
